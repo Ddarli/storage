805b4f6d05bf126deb62e687ceaf12a415eb48be
Ddarli <ivanlatyshev024@gmail.com>
2024-09-21 23:13:22 +0300

    third commit

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Ddarli/storage/internal/storage"
+	"log"
+)
 
+func main() {
+	st := storage.NewStorage()
+	file, err := st.SaveFile("text.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	restoredFile, err := st.GetByID(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("it is restored:", restoredFile)
 }
